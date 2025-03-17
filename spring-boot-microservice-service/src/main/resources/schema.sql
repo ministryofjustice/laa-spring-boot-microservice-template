f7a6e25a3fee0357c81b1e0b513e9c4016ea6b04
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-03-17 12:09:52 +0000

    Added GITHUB_TOKEN env for workflow.

--- spring-boot-microservice-service/src/main/resources/schema.sql
+++ spring-boot-microservice-service/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ITEMS
 (
-    id          BIGINT        AUTO_INCREMENT,
-    name        varchar(20)  NOT NULL,
-    description varchar(100) NOT NULL,
+    id          BIGINT          AUTO_INCREMENT,
+    name        varchar(20)     NOT NULL,
+    description varchar(100)    NOT NULL,
     PRIMARY KEY (id)
 );
